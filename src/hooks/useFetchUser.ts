ddfb6942c4a27762364062656aac60553ab37278
Kushan Gohil <kushanrocks.1311@gmail.com>
2026-05-08 22:28:04 +0530

    Changes in userFectch hook

--- src/hooks/useFetchUser.ts
+++ src/hooks/useFetchUser.ts
@@ -4,10 +4,11 @@ import { useUser } from "@clerk/nextjs";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { login } from "@/redux/slices/authSlice";
-import Cookies from "js-cookie";
+import { setOrganization } from "@/redux/slices/orgSlice";
 
 export default function useFetchUser() {
   const reduxUser = useAppSelector((state) => state.auth.user);
+  const currentOrg = useAppSelector((state) => state.organization.currentOrg);
   const { isSignedIn, user: clerkUser } = useUser();
   const dispatch = useDispatch();
 
@@ -16,26 +17,28 @@ export default function useFetchUser() {
       if (isSignedIn && clerkUser && !reduxUser) {
         try {
           const res = await fetch("/api/get-user");
+
+          if (!res.ok) {
+            console.error("Failed to fetch user:", res.status);
+            return;
+          }
+
           const data = await res.json();
-
           const userData = data as UserType;
-          Cookies.set("userToken", userData.id, { expires: 7 });
-
-          let resolvedOrgId = "";
-          if (userData.membership?.organizationId) {
-            resolvedOrgId = userData.membership.organizationId;
-          } else if (
-            userData.ownedOrganizations &&
-            userData.ownedOrganizations.length > 0
-          ) {
-            resolvedOrgId = userData.ownedOrganizations[0].id;
-          }
-
-          if (resolvedOrgId) {
-            Cookies.set("orgId", resolvedOrgId, { expires: 7 });
-          }
 
           dispatch(login(userData));
+
+          // Set default org in Redux if not already set
+          if (!currentOrg) {
+            const defaultOrg =
+              userData.ownedOrganizations?.[0] ??
+              userData.membership?.[0]?.organization ??
+              null;
+
+            if (defaultOrg) {
+              dispatch(setOrganization(defaultOrg));
+            }
+          }
         } catch (err) {
           console.error("Error fetching user:", err);
         }
